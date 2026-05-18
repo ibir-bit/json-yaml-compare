821ae9f1083c71635229507da5a9e9004d32d74a
Ivan Bir <ivbir.job@gmail.com>
2026-05-18 19:19:14 +0000

    Fix main.go

--- cmd/gendiff/code.go
+++
@@ -1,7 +0,0 @@
-package code
-
-import "code/pkg/gendiff"
-
-// GenDiff — это обертка над твоей основной функцией,
-// чтобы тесты Хекслета могли найти её в пакете 'code'
-var GenDiff = gendiff.GenDiff
