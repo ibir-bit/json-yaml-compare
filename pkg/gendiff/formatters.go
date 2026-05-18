ce6336825a5ba5c79e376c9eefde1da4b85f6abf
Ivan Bir <ivbir.job@gmail.com>
2026-05-18 12:36:59 +0000

    Fix map

--- pkg/gendiff/formatters.go
+++ pkg/gendiff/formatters.go
@@ -8,25 +8,25 @@ import (
 // formatStylish форматирует diff в красивый стиль stylish
 func formatStylish(nodes []DiffNode, depth int) string {
 	indent := func(depth int) string {
-		return strings.Repeat("  ", depth)
+		return strings.Repeat("    ", depth)
 	}
 
 	result := "{\n"
 	for _, node := range nodes {
 		switch node.Status {
 		case "nested":
-			result += fmt.Sprintf("%s  %s: %s", indent(depth-1), node.Key, formatStylish(node.Children, depth+1))
+			result += fmt.Sprintf("%s%s: %s", indent(depth), node.Key, formatStylish(node.Children, depth+1))
 		case "unchanged":
-			result += fmt.Sprintf("%s  %s: %v\n", indent(depth-1), node.Key, node.Value)
+			result += fmt.Sprintf("%s%s: %v\n", indent(depth), node.Key, node.Value)
 		case "added":
-			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth-1), node.Key, node.Value)
+			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth), node.Key, node.Value)
 		case "removed":
-			result += fmt.Sprintf("%s- %s: %v\n", indent(depth-1), node.Key, node.Value)
+			result += fmt.Sprintf("%s- %s: %v\n", indent(depth), node.Key, node.Value)
 		case "changed":
-			result += fmt.Sprintf("%s- %s: %v\n", indent(depth-1), node.Key, node.OldValue)
-			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth-1), node.Key, node.NewValue)
+			result += fmt.Sprintf("%s- %s: %v\n", indent(depth), node.Key, node.OldValue)
+			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth), node.Key, node.NewValue)
 		}
 	}
-	result += indent(depth-1) + "}\n"
+	result += indent(depth-1) + "}"
 	return result
 }
