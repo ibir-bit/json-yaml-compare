c4b2abb7c7b35f909245d7ab5ef305251565e5d4
Ivan Bir <ivbir.job@gmail.com>
2026-05-18 12:42:10 +0000

    Fix map

--- pkg/gendiff/formatters.go
+++ pkg/gendiff/formatters.go
@@ -2,47 +2,56 @@ package gendiff
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
-func formatValue(value interface{}) string {
-	switch v := value.(type) {
-	case map[string]interface{}:
-		if len(v) == 0 {
-			return "{}"
-		}
-		result := "{"
-		for k, val := range v {
-			result += fmt.Sprintf(" %s: %s", k, formatValue(val))
-		}
-		return result + " }"
-	default:
-		if v == nil {
-			return "null"
-		}
-		return fmt.Sprintf("%v", v)
-	}
-}
-
+// formatStylish форматирует diff в красивый стиль stylish
 func formatStylish(nodes []DiffNode, depth int) string {
 	indent := func(level int) string {
 		return strings.Repeat("    ", level)
+	}
+
+	var formatValue func(v interface{}, depth int) string
+	formatValue = func(v interface{}, depth int) string {
+		switch val := v.(type) {
+		case map[string]interface{}:
+			if len(val) == 0 {
+				return "{}"
+			}
+			keys := make([]string, 0, len(val))
+			for k := range val {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+
+			result := "{\n"
+			for _, k := range keys {
+				result += fmt.Sprintf("%s    %s: %s\n", strings.Repeat("    ", depth), k, formatValue(val[k], depth+1))
+			}
+			result += indent(depth) + "}"
+			return result
+		case nil:
+			return "null"
+		default:
+			return fmt.Sprintf("%v", val)
+		}
 	}
 
 	result := "{\n"
 	for _, node := range nodes {
 		switch node.Status {
 		case "nested":
-			result += fmt.Sprintf("%s%s: %s", indent(depth), node.Key, formatStylish(node.Children, depth+1))
+			result += fmt.Sprintf("%s    %s: %s\n", indent(depth), node.Key, formatStylish(node.Children, depth+1))
 		case "unchanged":
-			result += fmt.Sprintf("%s%s: %s\n", indent(depth), node.Key, formatValue(node.Value))
+			result += fmt.Sprintf("%s    %s: %s\n", indent(depth), node.Key, formatValue(node.Value, depth+1))
 		case "added":
-			result += fmt.Sprintf("%s+ %s: %s\n", indent(depth), node.Key, formatValue(node.Value))
+			result += fmt.Sprintf("%s  + %s: %s\n", indent(depth), node.Key, formatValue(node.Value, depth+1))
 		case "removed":
-			result += fmt.Sprintf("%s- %s: %s\n", indent(depth), node.Key, formatValue(node.Value))
+			result += fmt.Sprintf("%s  - %s: %s\n", indent(depth), node.Key, formatValue(node.Value, depth+1))
 		case "changed":
-			result += fmt.Sprintf("%s- %s: %s\n", indent(depth), node.Key, formatValue(node.OldValue))
-			result += fmt.Sprintf("%s+ %s: %s\n", indent(depth), node.Key, formatValue(node.NewValue))
+			result += fmt.Sprintf("%s  - %s: %s\n", indent(depth), node.Key, formatValue(node.OldValue, depth+1))
+			result += fmt.Sprintf("%s  + %s: %s\n", indent(depth), node.Key, formatValue(node.NewValue, depth+1))
 		}
 	}
 	result += indent(depth-1) + "}"
