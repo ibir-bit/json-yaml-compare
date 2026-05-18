f6a1c8cdaba96c69e7d8cd7027fe81d5e3ca55e8
Ivan Bir <ivbir.job@gmail.com>
2026-05-18 12:38:41 +0000

    Fix map

--- pkg/gendiff/formatters.go
+++ pkg/gendiff/formatters.go
@@ -5,10 +5,28 @@ import (
 	"strings"
 )
 
-// formatStylish форматирует diff в красивый стиль stylish
+func formatValue(value interface{}) string {
+	switch v := value.(type) {
+	case map[string]interface{}:
+		if len(v) == 0 {
+			return "{}"
+		}
+		result := "{"
+		for k, val := range v {
+			result += fmt.Sprintf(" %s: %s", k, formatValue(val))
+		}
+		return result + " }"
+	default:
+		if v == nil {
+			return "null"
+		}
+		return fmt.Sprintf("%v", v)
+	}
+}
+
 func formatStylish(nodes []DiffNode, depth int) string {
-	indent := func(depth int) string {
-		return strings.Repeat("    ", depth)
+	indent := func(level int) string {
+		return strings.Repeat("    ", level)
 	}
 
 	result := "{\n"
@@ -17,14 +35,14 @@ func formatStylish(nodes []DiffNode, depth int) string {
 		case "nested":
 			result += fmt.Sprintf("%s%s: %s", indent(depth), node.Key, formatStylish(node.Children, depth+1))
 		case "unchanged":
-			result += fmt.Sprintf("%s%s: %v\n", indent(depth), node.Key, node.Value)
+			result += fmt.Sprintf("%s%s: %s\n", indent(depth), node.Key, formatValue(node.Value))
 		case "added":
-			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth), node.Key, node.Value)
+			result += fmt.Sprintf("%s+ %s: %s\n", indent(depth), node.Key, formatValue(node.Value))
 		case "removed":
-			result += fmt.Sprintf("%s- %s: %v\n", indent(depth), node.Key, node.Value)
+			result += fmt.Sprintf("%s- %s: %s\n", indent(depth), node.Key, formatValue(node.Value))
 		case "changed":
-			result += fmt.Sprintf("%s- %s: %v\n", indent(depth), node.Key, node.OldValue)
-			result += fmt.Sprintf("%s+ %s: %v\n", indent(depth), node.Key, node.NewValue)
+			result += fmt.Sprintf("%s- %s: %s\n", indent(depth), node.Key, formatValue(node.OldValue))
+			result += fmt.Sprintf("%s+ %s: %s\n", indent(depth), node.Key, formatValue(node.NewValue))
 		}
 	}
 	result += indent(depth-1) + "}"
