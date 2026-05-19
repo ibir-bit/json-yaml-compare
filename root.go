9560054479d32c73da900fcd0f07c2fcc1e00a29
Ivan Bir <ivbir.job@gmail.com>
2026-05-19 00:38:09 +0000

    Fix bug

--- root.go
+++ root.go
@@ -1,48 +1,29 @@
 package code
 
 import (
-	"os"
-
 	"code/pkg/gendiff" // Убедитесь, что здесь ваш правильный импорт пакета
-
-	"gopkg.in/yaml.v3"
 )
 
 // GenDiff читает два файла, сравнивает их и возвращает результат в виде строки.
 // Именно эту функцию ищут тесты Хекслета.
-func GenDiff(path1, path2 string) (string, error) {
-	// 1. Читаем и парсим первый файл
+// Добавьте третий параметр 'format'
+func GenDiff(path1, path2 string, format string) (string, error) {
+	// Временно игнорируем формат, если он еще не реализован в логике
+	_ = format
+
+	// 1. Читаем и парсим файлы (код, который вы уже написали)
 	data1, err := readAndParse(path1)
 	if err != nil {
 		return "", err
 	}
 
-	// 2. Читаем и парсим второй файл
 	data2, err := readAndParse(path2)
 	if err != nil {
 		return "", err
 	}
 
-	// 3. Вызываем вашу внутреннюю функцию, которая делает всю магию
-	// (У вас она называлась GenDiffRecursive)
+	// 2. Вызываем основную логику
 	diff := gendiff.GenDiffRecursive(data1, data2)
 
 	return diff, nil
-}
-
-// Эту функцию мы уже писали для main.go, ее можно перенести сюда,
-// чтобы не дублировать код, а из main.go просто вызывать code.GenDiff(path1, path2)
-func readAndParse(filepath string) (map[string]interface{}, error) {
-	content, err := os.ReadFile(filepath)
-	if err != nil {
-		return nil, err
-	}
-
-	var data map[string]interface{}
-	err = yaml.Unmarshal(content, &data)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
 }
